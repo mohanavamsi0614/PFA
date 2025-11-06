cd9597ae562e370320ff145b11b3919439b59cae
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-11-06 22:09:57 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/rounds", roundsRouter);
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 connectDB();
 
